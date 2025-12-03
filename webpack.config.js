bf8bc6ff146ce5bc3bf909da9838480dfdc8b7e9
Simone Picciani <simone.picciani@prima.it>
2025-12-03 18:47:40 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "showcase",
+    projectName: "shell",
+    webpackConfigEnv,
+    argv,
+    outputSystemJS: false,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
